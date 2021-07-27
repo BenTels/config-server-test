ac207242576b311fa6cfb4da5cb2a5c27cd05faf
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-07-27 16:19:51 +0200

    Basic example

---
+++ service/src/main/java/nl/bentels/conserv/test/MessageBlastingController.java
@@ -0,0 +1,19 @@
+package nl.bentels.conserv.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageBlastingController {
+
+	@Value("${message}")
+	private String message;
+
+	@GetMapping(path = "/")
+	public String getMessage() {
+		return message;
+	}
+}
